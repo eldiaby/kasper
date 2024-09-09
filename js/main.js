41bf35f91f2028f1d387c6d9bd4a2317f6c21017
unknown <elgdiab@gmail.com>
2024-09-09 15:59:41 +0300

    working in the mobile media query but not finnish yet

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*"use strict";
+
+const [navIconOpen, navIconClose] = document.querySelectorAll(".nav-icon");
+
+navIconOpen.addEventListener("click", (e) => {
+  navIconOpen.style.display = "none";
+  //styly.display = "none";
+});
+*/
